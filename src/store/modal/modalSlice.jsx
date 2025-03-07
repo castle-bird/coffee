496ddd447a247fd57cb54215fe6a9cc03fdbd739
castle-bird <skswkfrjdi12@gmail.com>
2025-03-07 14:33:34 +0900

    안내 추가가

--- src/store/modal/modalSlice.jsx
+++ src/store/modal/modalSlice.jsx
@@ -12,6 +12,10 @@ const modalSlice = createSlice({
              * 장바구니 보기 = showCart
              *
              * 결제 보기 = showPayment
+             * 
+             * 메뉴 상세 보기 = showDetail
+             * 
+             * 팝업 닫기는 그냥 빈 문자열 = ""
              */
         },
     },
