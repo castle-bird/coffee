24c49f4e3659133499ec825d64079d7e268d81cd
castle-bird <skswkfrjdi12@gmail.com>
2025-03-11 13:07:47 +0900

    ㅁㄴㅇㅇ

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import GlobalStyle from "./globalStyle/GlobalStyle";
+import GlobalStyle from "./global-style/GlobalStyle.jsx";
 
 createRoot(document.getElementById("root")).render(
     <>
